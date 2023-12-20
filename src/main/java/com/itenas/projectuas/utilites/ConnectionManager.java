af7d594330e3d462ed243b705f4a248caa419682
RizalAhmadSaepulloh <rizalahmadsaepulloh@gmail.com>
2023-12-21 06:35:02 +0700

    menambahkan Connection Manager

--- src/main/java/com/itenas/projectuas/utilites/ConnectionManager.java
+++ src/main/java/com/itenas/projectuas/utilites/ConnectionManager.java
@@ -4,10 +4,36 @@
  */
 package com.itenas.projectuas.utilites;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author rizal
  */
 public class ConnectionManager {
-    
+
+    private Connection con;
+    private String driver = "com.mysql.jdbc.Driver";
+    private String url = "jdbc:mysql://localhost:3306/database_w10";
+    private String username = "root";
+    private String password = "";
+
+    public Connection LogOn() {
+        try {
+            Class.forName(driver).newInstance();
+            con = DriverManager.getConnection(url, username, password);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return con;
+    }
+
+    public void LogOff() {
+        try {
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
