f6891cde75d253abcf269fd0ab120d1239eaf087
RizalAhmadSaepulloh <rizalahmadsaepulloh@gmail.com>
2023-12-21 06:42:13 +0700

    cek pull

--- src/main/java/com/itenas/projectuas/controller/NewClass.java
+++ src/main/java/com/itenas/projectuas/controller/NewClass.java
@@ -9,5 +9,7 @@ package com.itenas.projectuas.controller;
  * @author rizal
  */
 public class NewClass {
-    
+    public static void main(String[] args) {
+        System.out.println("cek");
+    }
 }
